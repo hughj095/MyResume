9a81b26ba82f0308d732a2ad7b56c2fe8eed5faf
hughj095 <john.m.hughes84@outlook.com>
2024-03-13 13:21:24 -0400

    jh

--- portfolio.py
+++ portfolio.py
@@ -1,26 +1,50 @@
 # Alpha Vantage API
 
 import requests
-
-stock1 = 'MCD'
-stock2 = 'DG'
-stock3 = 'COIN'
-stock4 = 'HD'
-stock5 = 'NVDA'
+import csv
+import pandas as pd
 
 API_KEY = 'LNR6C1L773RCAOFY'
 
-# for loop for each ticker and append to dataframe with ticker, open, close, net, shares, total
+# Create empty list of csv columns
+ticker_column = []
+shares_column = []
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock1}&interval=5min&apikey={API_KEY}'
-r = requests.get(url)
-data = r.json()
+# Open csv, find the last row, and append values to empty lists
+with open('stocks.csv', mode='r') as file:
+    reader = csv.reader(file)
+    last_used_row = None
+    for row in reversed(list(reader)):
+        if row[0] != "":
+            last_used_row = row
+            break
+    file.seek(0)
+    for row in reader:
+        if row[0] != "":
+            ticker_column.append(row[0])
+            shares_column.append(row[1])
 
-# need only the first value here open and close
-print(data['Time Series (5min)'][0])
+# dataframe the data and loop for each ticker and append to dataframe with close and total
+data = {'Ticker':ticker_column[1:], 'Shares':shares_column[1:]}
+df = pd.DataFrame(data)
+x=0
+for ticker in df['Ticker']:
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval=5min&apikey={API_KEY}'
+    r = requests.get(url)
+    data = r.json()
+    print(data)
+    time = data['Time Series (5min)']
+    for timestamp, values in time.items():
+        if '4. close' in values:
+            close = values['4. close']
+            break
+    df.iloc[x, 2] = close
+    x += 1
 
-# append to dataframe
+# Check final df
+print(df)
+
+# Calculate total in each row and total portfolio
 
 # Twilio info to text me daily updates
 
