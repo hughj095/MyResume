e14a35098f0450d8ae78c1c29799c771634e188e
hughj095 <john.m.hughes84@outlook.com>
2024-03-20 18:38:51 -0400

    jh

---
+++ functions.py
@@ -0,0 +1,13 @@
+
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    
+    return f'{formatted_f_name} {formatted_l_name}'
+
+string = format_name('john', 'JOHN')
+print(string)
+
+
+
