cd7fea46eef9c7a3c4ce80febba8745ed626b048
John <john.m.hughes84@outlook.com>
2024-05-03 20:34:58 -0400

    jh

---
+++ portfolio2.0.py
@@ -0,0 +1,24 @@
+# imports and variables
+
+TICKER = 'AAPL'
+MULTIPLIER = '1'
+TIMESPAN = 'day'
+DATESTART = '05-03-24'
+DATEEND = '05-03-24'
+APIKEY = 'hldKpOcQ9ago1ZA83XRyRQ1tFG5uokBa'
+
+url = f'https://api.polygon.io/v2/aggs/ticker/{TICKER}/range/{MULTIPLIER}/{TIMESPAN}/{DATESTART}/{DATEEND}?apiKey={APIKEY}'
+
+# get all stock info
+
+# API keys and authorizations
+
+# API pull data
+
+# indicators and management
+
+# order placement, goal posts and transaction history
+
+# notifications
+
+# testing and alerts
