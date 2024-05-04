cf2a57247c70ef860531f3551a1bb945fce98761
John <john.m.hughes84@outlook.com>
2024-05-03 20:55:32 -0400

    jh

--- portfolio2.0.py
+++ portfolio2.0.py
@@ -1,17 +1,22 @@
 # imports and variables
+import requests
 
 TICKER = 'AAPL'
 MULTIPLIER = '1'
 TIMESPAN = 'day'
-DATESTART = '05-03-24'
-DATEEND = '05-03-24'
+DATESTART = '2024-05-02'
+DATEEND = '2024-05-02'
 APIKEY = 'hldKpOcQ9ago1ZA83XRyRQ1tFG5uokBa'
 
 url = f'https://api.polygon.io/v2/aggs/ticker/{TICKER}/range/{MULTIPLIER}/{TIMESPAN}/{DATESTART}/{DATEEND}?apiKey={APIKEY}'
 
 # get all stock info
-
-# API keys and authorizations
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print('Error:', response.status_code)
 
 # API pull data
 
