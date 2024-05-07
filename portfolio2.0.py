5dac9f45021c6a50cb109b7f445bbb3915646745
John <john.m.hughes84@outlook.com>
2024-05-07 06:28:20 -0400

    JH

--- portfolio2.0.py
+++ portfolio2.0.py
@@ -10,7 +10,9 @@ TIMESPAN = 'minute'
 DATESTART = '2024-05-03'
 DATEEND = '2024-05-03'
 APIKEY = 'hldKpOcQ9ago1ZA83XRyRQ1tFG5uokBa'
-SHARES = int(4)
+SHARES = 4
+
+current_time = datetime.datetime.now().time()
 
 def get_ticker_data(TICKER, MULTIPLIER, TIMESPAN, DATESTART, DATEEND, APIKEY):
     url = f'https://api.polygon.io/v2/aggs/ticker/{TICKER}/range/{MULTIPLIER}/{TIMESPAN}/{DATESTART}/{DATEEND}?apiKey={APIKEY}'
@@ -22,49 +24,55 @@ def get_ticker_data(TICKER, MULTIPLIER, TIMESPAN, DATESTART, DATEEND, APIKEY):
         print('Error:', response.status_code, response.content)
         return None
 
-def buy_stock(TICKER, strike_price, buy_time, SHARES):
+##### BUY FUNCTION WANTS TO START OVER AGAIN BUT NEEDS TO APPEND TO THE LAST TRANSACTION
+##### check the HOLD FUNCTION TO BREAK OUT OF THE WHILE LOOP
+
+def buy_stock(TICKER, strike_price, buy_time, SHARES, current_time):
+    total = strike_price*SHARES
     buy_data = {'Ticker': [TICKER],
                 'Strike Price': [strike_price],
                 'Buy Time': [buy_time],
                 'Shares': [SHARES],
-                'Total': [strike_price*SHARES]}
+                'Buy Total': [total],
+                'Sell Price': '',
+                'Sell Time': '',
+                'Total': '',
+                'Net': '',
+                'Current Time': ''}
     df_transactions = pd.DataFrame(buy_data)
-    df_transactions.to_csv(r'C:\Users\johnm\OneDrive\Desktop\MyResume\transactions.csv', mode='a', header=False, index=False)
-    hold_stock(df_transactions, buy)
+    hold_stock(df_transactions, buy, current_time, SHARES, strike_price)
 
-def hold_stock(df_transactions, buy):
+def hold_stock(df_transactions, buy, current_time, SHARES, strike_price):
     sell = False
     while buy == True:
         # start timer to match multiplier and interval
+        current_time = datetime.datetime.now().time()
         # if nearing timer then:
             # run API and
-            for i in range(len(df)):
-                if df.iloc[i,8] == 'resistance' and df.iloc[i,9] == '' and buy_time < df.iloc[i,6]:
-                    sell = True
-                    sell_price = df.iloc[i,3]
-                    df_transactions.iloc[i,5] = sell_price
-                    sell_time = df.iloc[i,6]
-                    buy = False
-                    sell_stock(TICKER, sell_stock, sell_time, SHARES)
-            # if time is 3:55pm then sell
-
-     
-
-def sell_stock(TICKER, sell_price, sell_time, SHARES):
-    # sell stock
-    # 
+        x=0
+        for i in range(len(df)):
+            if df.iloc[i,8] == 'resistance' and df.iloc[i,9] == '' and buy_time < df.iloc[i,6]:
+                sell_time = df.iloc[i,6]
+                sell_price = df.iloc[i,3]
+                sell_stock(df_transactions, sell_time, sell_price, SHARES, current_time, strike_price, x)
+                x+=1
+                sell = True
+                buy = False
+        current_time = datetime.datetime.now().time()
+        if current_time > datetime.time(15, 54):
+                sell_stock(TICKER, df_transactions, SHARES, strike_price, sell_time=sell_time,  current_time=current_time, sell_price=df.iloc[i,3])
+            
+def sell_stock(df_transactions, sell_time, sell_price, SHARES, current_time, strike_price, x):
+    df_transactions.iloc[x,5] = sell_price
+    df_transactions.iloc[x,6] = sell_time
+    df_transactions.iloc[x,7] = sell_price * SHARES
+    df_transactions.iloc[x,8] = df_transactions.iloc[x,7] - df_transactions.iloc[x,4]
+    df_transactions.iloc[x,9] = current_time
+    df_transactions.to_csv(r'C:\Users\johnm\OneDrive\Desktop\MyResume\transactions.csv', mode='a', header=False, index=False)
 
 ### API pull data
 
 data = get_ticker_data(TICKER, MULTIPLIER, TIMESPAN, DATESTART, DATEEND, APIKEY)
-
-'''url = f'https://api.polygon.io/v2/aggs/ticker/{TICKER}/range/{MULTIPLIER}/{TIMESPAN}/{DATESTART}/{DATEEND}?apiKey={APIKEY}'
-response = requests.get(url)
-if response.status_code == 200:
-    data = response.json()
-    #pprint.pprint(data)
-else:
-    print('Error:', response.status_code, response.content)'''
 
 ### indicators and management
 
@@ -121,7 +129,7 @@ for i in range(len(df)):
         strike_price = df.iloc[i,3]
         buy = True
         buy_time = df.iloc[i,6]
-        buy_stock(TICKER, strike_price, buy_time, SHARES)
+        buy_stock(TICKER, strike_price, buy_time, SHARES, current_time)
 
 
 
