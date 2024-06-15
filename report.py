0c76d3a7feb881ed7a9ad9bdffaa995919047a9f
John <john.m.hughes84@outlook.com>
2024-06-15 14:14:28 -0400

    jh

---
+++ report.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+class Report:
+    def report():
+        df_report = pd.readcsv('')
+        # open new tab/sheet
+        # pull executions for day
+        # summarize by ticker with columns Bought and Sold
+        # Net for day by ticker
+        # Total for day
+        # Diff vs previous day
+        # YTD Net
