cac0476c902d8c19ab591ddbfa0e087948743b67
John <john.m.hughes84@outlook.com>
2024-06-15 14:19:10 -0400

    jh

--- report.py
+++ report.py
@@ -1,10 +1,14 @@
 import pandas as pd
+from ib_insync import *
 
 class Report:
-    def report():
+    def report(ib):
         df_report = pd.readcsv('')
         # open new tab/sheet
         # pull executions for day
+        executions = ib.reqExecutions(ExecutionFilter())
+        # put execution items in a df
+        # adjust IB time to my time
         # summarize by ticker with columns Bought and Sold
         # Net for day by ticker
         # Total for day
