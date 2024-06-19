0301f22b3d5a8643fe5720717d4558790d7a1b96
John <john.m.hughes84@outlook.com>
2024-06-19 18:57:30 -0400

    jh

---
+++ ingest_to_sql.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+
+class Upload_To_SQL:
+    def upload():
+        # Replace these with your actual database details
+        server = 'your_server.database.windows.net'
+        database = 'your_database'
+        username = 'your_username'
+        password = 'your_password'
+        driver = 'ODBC Driver 17 for SQL Server'
+
+        # Connection string
+        connection_string = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}'
+
+        # Create engine
+        engine = create_engine(connection_string)
